37c27d75386ca4c6a413437b33fbc4fd7994f8e9
Tirtharaj sen <tirtharajsen524@gmail.com>
2024-02-18 14:09:26 +0530

    code

--- components/UsersList.js
+++ components/UsersList.js
@@ -2,10 +2,19 @@ import { useMemo } from 'react';
 
 function UsersList({ users, startingLetter }) {
   
+  const filterUser = useMemo(()=>{
+    console.log("UsersList");
+return users.filter((obj)=>obj.name.toLowerCase().startsWith(startingLetter.toLowerCase()))},
+[users,startingLetter])
 
   return (
     <ul>
-      <li key={}>{}</li>
+    {filterUser.map((obj)=>{
+        return (
+             <li key={obj.id}>{obj.name}</li>
+        )
+    })}
+     
     </ul>
   );
 }
